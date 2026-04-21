70d56d803c16a77d18d325dd5aa307e1569f7333
mjohnson20121 <mjohnson20121@mail.cfcc.edu>
2026-04-21 18:09:20 -0400

    Stored Procedures

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Building a Database with Stored Procedures</title>
+    <link rel="stylesheet" href="styles\main.css">
+</head>
+<body>
+    <h1>Building a Database with Stored Procedures</h1>
+    <p>Michael Johnson</p>
+    <p>04/20/2026</p>
+    
+</body>
+</html>
